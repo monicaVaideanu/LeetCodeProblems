660a91004871288ec1925ea3658376804c78cd3c
monicaVaideanu <123547873+monicaVaideanu@users.noreply.github.com>
2023-01-26 13:31:34 +0200

    Contains Duplicate

---
+++ 217. Contains Duplicare/ContainsDuplicate.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+
+/**
+ * https://leetcode.com/problems/contains-duplicate/
+ * Given an integer array nums, return true if any value appears at least twice in the array, and return false if every
+ * element is distinct.
+ */
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            } else {
+                set.add(nums[i]);
+            }
+        }
+        return false;
+    }
+}
