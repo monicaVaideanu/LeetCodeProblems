8bf856135ee897cbbd11de6f2f4d985b41be1b69
monicaVaideanu <123547873+monicaVaideanu@users.noreply.github.com>
2023-01-27 11:32:01 +0200

    Remove Duplicates from Sorted Array

---
+++ 26. Remove Duplicates from Sorted Array/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+ * Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique
+ * element appears only once. The relative order of the elements should be kept the same.
+ * Since it is impossible to change the length of the array in some languages, you must instead have the result be
+ * placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates,
+ * then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k
+ * elements. Return k after placing the final result in the first k slots of nums.
+ * Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1)
+ * extra memory.
+ * <p>
+ * Custom Judge:
+ * <p>
+ * The judge will test your solution with the following code:
+ * <p>
+ * int[] nums = [...]; // Input array
+ * int[] expectedNums = [...]; // The expected answer with correct length
+ * <p>
+ * int k = removeDuplicates(nums); // Calls your implementation
+ * <p>
+ * assert k == expectedNums.length;
+ * for (int i = 0; i < k; i++) {
+ * assert nums[i] == expectedNums[i];
+ * }
+ */
+public class RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        int k = nums.length;
+        for (int i = nums.length - 1; i > 0; i--) {
+            if (nums[i] == nums[i - 1]) {
+                k--;
+                nums[i] = 2322 + i;
+            }
+        }
+        Arrays.sort(nums);
+        return k;
+    }
+}
